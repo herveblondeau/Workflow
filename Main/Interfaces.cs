da9515986c9f343c7350bed878c5a3216065d1f4
Hervé Blondeau <herve.blondeau@protonmail.com>
2025-11-05 16:30:49 +0100

    Remove unused pipeline methods

--- Main/Interfaces.cs
+++ Main/Interfaces.cs
@@ -11,27 +11,12 @@ public struct Unit
     public static readonly Unit Value = new Unit();
 }
 
-// public enum ToolState
-// {
-//     Idle,
-//     Starting,
-//     Running,
-//     Stopping,
-// }
-
-public class MultiplierTool : ITool<int, int>
+public enum ToolState
 {
-    public int Multiplier { get; private set; }
-
-    public MultiplierTool(int multiplier)
-    {
-        Multiplier = multiplier;
-    }
-
-    public Task<int> Transform(int input, CancellationToken cancellationToken = default)
-    {
-        return Task.FromResult(input * Multiplier);
-    }
+    Idle,
+    Starting,
+    Running,
+    Stopping,
 }
 
 public interface IPipeline<TIn, TOut>
@@ -39,25 +24,23 @@ public interface IPipeline<TIn, TOut>
     Task<TOut> Execute(TIn input, CancellationToken cancellationToken = default);
 }
 
-public class Pipeline<TIn, TOut> : IPipeline<TIn, TOut>, ITool<TIn, TOut>
+public class Pipeline<TIn, TOut> : IPipeline<TIn, TOut>
 {
     private readonly Func<TIn, CancellationToken, Task<TOut>> _executor;
 
     private Pipeline(Func<TIn, CancellationToken, Task<TOut>> executor) { _executor = executor; }
 
-    public Task<TOut> Execute(TIn input, CancellationToken cancellationToken = default) => _executor(input, cancellationToken);
-
-    // Convenience overload for no-input pipelines
-    public Task<TOut> Execute(CancellationToken cancellationToken = default)
+    public Task<TOut> Execute(TIn input, CancellationToken cancellationToken = default)
     {
-        if (typeof(TIn) != typeof(Unit))
-            throw new InvalidOperationException("This overload can only be used for pipelines with Unit input.");
+        return _executor(input, cancellationToken);
+    }
+
+    public Task<TOut> Execute(CancellationToken cancellationToken = default) // Convenience overload for no-input pipelines
+    {
         return _executor((TIn)(object)Unit.Value, cancellationToken);
     }
 
-    public Task<TOut> Transform(TIn input, CancellationToken cancellationToken = default) => _executor(input, cancellationToken);
-
-    // Add another tool to the pipeline
+    // Chain another tool to the pipeline
     public Pipeline<TIn, TNext> Add<TNext>(ITool<TOut, TNext> next) => new Pipeline<TIn, TNext>(
     async (input, cancellationToken) =>
     {
@@ -72,8 +55,5 @@ public class Pipeline<TIn, TOut> : IPipeline<TIn, TOut>, ITool<TIn, TOut>
 public static class Pipeline
 {
     public static Pipeline<TIn, TOut> Add<TIn, TOut>(ITool<TIn, TOut> tool) => Pipeline<TIn, TOut>.Create(tool);
-
-    // For tools that don't take input (i.e., ITool<Unit, TOut>)
-    public static Pipeline<Unit, TOut> Start<TOut>(ITool<Unit, TOut> tool)
-        => Pipeline<Unit, TOut>.Create(tool);
+    public static Pipeline<Unit, TOut> Add<TOut>(ITool<Unit, TOut> tool) => Pipeline<Unit, TOut>.Create(tool); // For tools that don't take input (i.e., ITool<Unit, TOut>)
 }
